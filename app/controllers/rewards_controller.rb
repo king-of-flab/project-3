1b290cb6401caa4583d12978b102566af4f05722
shumin huang <ms.huang.shumin@gmail.com>
2017-08-10 21:48:33 +0800

    RESTful routes for requests and rewards

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,13 +1,15 @@
 class RewardsController < ApplicationController
 
+  before_action :authenticate_account!, except: [:index, :show]
+
   def index
     @all_rewards = Reward.all
   end
 
   def create
-    @new_reward = Reward.create(params.require(:reward).permit!)
-    @new_reward.created_by = current_account.id
-    redirect_to reward_path(@new_reward) if @new_reward.save
+    new_reward = Reward.create(reward_params)
+    new_reward.created_by = current_account.id
+    redirect_to reward_path(new_reward) if new_reward.save
   end
 
   def new
@@ -24,13 +26,23 @@ class RewardsController < ApplicationController
   end
 
   def update
-
+    updated_reward = Reward.find(params[:id])
+    updated_reward.create(reward_params)
+    redirect_to reward_path(updated_reward) if updated_reward.save
   end
 
   def destroy
+    Reward.destroy(params[:id])
+    redirect_to rewards_path
+    # redirect to profile page once profile page is up
+    # place the delete button in the profile page
+    # <%= button_to "delete", reward_path(@current_reward), method: :delete, class: "btn waves-effect waves-light" %>
   end
 
-  def reward_update_params
+  private
+
+  def reward_params
+    params.require(:reward).permit(:name, :date, :start_time, :end_time, :address, :area, :opening, :unit_time_credit, :description, :image)
   end
 
 end
