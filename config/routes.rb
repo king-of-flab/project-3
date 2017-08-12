463b70d9aaf3febcba5bd05959378309a25378f1
shumin huang <ms.huang.shumin@gmail.com>
2017-08-12 22:02:52 +0800

    edit accounts controller, views, routes and seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
-  get 'accounts/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'main#index'
 
   devise_for :accounts
 
-  resources :requests do
-    resources :accounts, only: [:show], to: 'accounts#show'
-  end
+  resources :requests
+
+  resources :rewards
 
   get 'my_requests', to: 'accounts#requests'
   get 'my_requests/:id', to: 'accounts#single_request'
-
-  resources :rewards do
-    resources :accounts, only: [:show], to: 'accounts#show'
-  end
 
   get 'my_rewards', to: 'accounts#rewards'
   get 'my_rewards/:id', to: 'accounts#single_reward'
--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,11 +1,5 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get accounts_index_url
-  test "should get show" do
-    get accounts_show_url
-    assert_response :success
-  end
 
 end
