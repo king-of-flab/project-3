f683fbf3c3b26561571853b7eb1978a6d9641163
Daniel Lim <ddanielnp@hotmail.com>
2017-08-15 16:46:36 +0800

    edits

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -14,19 +14,18 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
     resource.save
     yield resource if block_given?
 
-    new_account = Account.find_by(email: params[:account][:email])
-    account_type = new_account[:account_type]
-
-    if account_type === "individual"
-      new_account.time_credit = 0
-      new_account.save
-    elsif account_type === "organisation"
-      new_account.time_credit = 1000
-      new_account.save
-    end
-
     if resource.persisted?
       if resource.active_for_authentication?
+        new_account = Account.find_by(email: params[:account][:email])
+        account_type = new_account[:account_type]
+
+        if account_type === "individual"
+          new_account.time_credit = 0
+          new_account.save
+        elsif account_type === "organisation"
+          new_account.time_credit = 1000
+          new_account.save
+        end
 
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
