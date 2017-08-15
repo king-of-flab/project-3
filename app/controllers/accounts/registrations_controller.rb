278fa7b03c236ec71363d4f29d232c5be049b53f
king-of-flab <nawas@live.com.sg>
2017-08-15 10:29:36 +0800

    took out hidden field

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -13,8 +13,21 @@ class Accounts::RegistrationsController < Devise::RegistrationsController
 
     resource.save
     yield resource if block_given?
+
+    new_account = Account.find_by(email: params[:account][:email])
+    account_type = new_account[:account_type]
+
+    if account_type === "individual"
+      new_account.time_credit = 0
+      new_account.save
+    elsif account_type === "organisation"
+      new_account.time_credit = 1000
+      new_account.save
+    end
+
     if resource.persisted?
       if resource.active_for_authentication?
+
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
