a2441641c077cd9dd8e6cd9534e201b395c5b288
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-08-17 15:10:21 +0800

    included figaro

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -100,29 +100,29 @@ class RequestsController < ApplicationController
   end
 
   def send_text_message
-    numbers = []
+      numbers = []
 
-    @request.accounts.each do |account|
-      numbers << "+65#{account.tel}"
-    end
-
-    numbers.each do |number|
-      if ["+6592385117", "+6597926982", "+6598242708", "+6583666605", "+6598246595", "+6583387004"].include?(number)
-      number_to_send_to = number
-
-      twilio_sid = "ACf59cbcf94d9499de2bde0902b1a8f5eb"
-      twilio_token = "0f82d290b56df55ef4d2c3f08184610a"
-      twilio_phone_number = "+43676800200810"
-
-      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
-      @twilio_client.api.account.messages.create(
-        :from => "#{twilio_phone_number}",
-        :to => number_to_send_to,
-        :body => "#{@request.name} is coming up on #{@request.date.strftime('%d %B %Y (%A)')} at #{@request.start_time.strftime('%I:%M%p')}."
-      )
+      @request.accounts.each do |account|
+        numbers << "+65#{account.tel}"
       end
-    end
+
+      numbers.each do |number|
+        if ["+6592385117", "+6597926982", "+6598242708", "+6598246595", "+6583387004"].include?(number)
+        number_to_send_to = number
+
+        twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+        twilio_token = ENV['TWILIO_AUTH_TOKEN']
+        twilio_phone_number = ENV['TWILIO_NUMBER']
+
+        @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+        @twilio_client.api.account.messages.create(
+          :from => "#{twilio_phone_number}",
+          :to => number_to_send_to,
+          :body => "#{@request.name} is coming up on #{@request.date.strftime('%d %B %Y (%A)')} at #{@request.start_time.strftime('%I:%M%p')}."
+        )
+        end
+      end
   end
 
   private
